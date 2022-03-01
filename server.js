25c4693216fdc8ae3efac6e2d2270119fda24258
Gagan Singh <gagansingh.dev@outlook.com>
2022-03-01 12:49:34 -0500

    Basic dockerizing config using Batect

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 5111;
+const PORT = process.env.PORT || 5000;
 
 const server = require("./src/app")({
   logger: {
@@ -9,7 +9,7 @@ const server = require("./src/app")({
 
 const start = async () => {
   try {
-    await server.listen(PORT);
+    await server.listen(PORT, "0.0.0.0");
   } catch (error) {
     server.log.error(error);
     process.exit(1);
